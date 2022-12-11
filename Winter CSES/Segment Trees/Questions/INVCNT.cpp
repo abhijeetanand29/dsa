0c03161099811054cf2d3108d7160da13399a74f
abhijeetanand29 <abhijeet_a@me.iitr.ac.in>
2022-12-12 01:28:37 +0530

    Ques Added

---
+++ Winter CSES/Segment Trees/Questions/INVCNT.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define vv vector<int>
+#define ff first
+#define ss second
+#define pr pair<int,int>
+#define inp cin>>
+#define out cout<<
+#define el endl
+#define F(i,a,b) for(int i = a; i < b; i++)
+using namespace std;
+
+void solve(){
+    
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}
